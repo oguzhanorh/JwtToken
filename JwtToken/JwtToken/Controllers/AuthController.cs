783ee90e56306964bfb6f6b8ea24eda31f56f3cd
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2024-02-25 01:31:10 +0300

    create a token

--- JwtToken/JwtToken/Controllers/AuthController.cs
+++ JwtToken/JwtToken/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 ﻿using JwtToken.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace JwtToken.Controllers
 {
@@ -9,6 +13,11 @@ namespace JwtToken.Controllers
     public class AuthController : ControllerBase
     {
         public static User user = new User();
+        private readonly IConfiguration _configuration;
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost("register")]
         public ActionResult<User>Register(UserDto request)
@@ -34,7 +43,32 @@ namespace JwtToken.Controllers
             {
                 return BadRequest("Wrong password");
             }
-            return Ok(user);
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,user.UserName),
+
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!)); //uygulama ayarlarından anahtar alıyoruz.
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims:claims,
+                expires:DateTime.Now.AddDays(1),
+                signingCredentials:cred
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
     }
 }
